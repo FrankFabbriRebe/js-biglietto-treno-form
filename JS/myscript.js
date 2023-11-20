5ea98a5950698f40981b92570e0bdde5d014b190
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-11-20 17:03:35 +0100

    problem with parseInt

--- JS/myscript.js
+++ JS/myscript.js
@@ -1,40 +1,49 @@
 // slection prompt name
-const inputName = document.querySelector(".nome");
-console.log(inputName);
+// const inputName = document.querySelector(".nome");
+// console.log(inputName);
 
 // selction prompt age
-const inputAge = document.querySelector(".age");
-console.log(inputAge);
+// const inputAge = document.querySelector(".age");
+// console.log(inputAge);
 
 // selection prompt km viaggio
-const inputKm = document.querySelector(".distance")
+const inputKm = document.querySelector(".distance");
 console.log(inputKm);
+
 
 // selection button
 const sendButton = document.querySelector(".genera");
-console.log(sendButton);
+// console.log(sendButton);
 
 // creation some variables
-let userName;
+// let userName;
 
-let etaPassegero;
+// let etaPassegero;
 
 let kmViaggio;
+
+// calcolo costo biglietto senza sconti
+// let costoBigliettoIniziale = kmViaggio * 0.21;
+// console.log("costo del biglietto senza sconti:", costoBigliettoIniziale,"€");
 
 // evento al click
 sendButton.addEventListener("click", function()  {
     // assegnazione del valore a userName 
-    userName = inputName.value;
-    console.log(userName);
+    // userName = inputName.value;
+    // console.log(userName);
     // assegnazione del valore a etaPassegero
-    etaPassegero = parseInt(inputAge.value);
-    console.log(etaPassegero);
+    // etaPassegero = parseInt(inputAge.value);
+    // console.log(etaPassegero);
     // assegnazione del valore a kmViaggio
     kmViaggio = parseInt(inputKm.value);
     console.log(kmViaggio);
 });
 
-
+// if (isNaN(kmViaggio)) {
+//     console.error("L'input non è un numero valido");
+// } else {
+//     console.log("Valore valido: " + kmViaggio);
+// }
 
 
 
